a226620e9af4da9ced3e5a8835df25a0c5b66326
kojo <kojo.abakah@techconsulting.tech>
2023-10-25 00:49:32 -0400

    working on models

--- WeatherApp/Models/Weather.swift
+++ WeatherApp/Models/Weather.swift
@@ -8,5 +8,45 @@
 import Foundation
 
 struct Weather: Codable, Identifiable {
+    var dt: Int
+    var temp: Double
+    var feel_like: Double
+    var pressure: Int
+    var humidity: Int
+    var dew_point: Double
+    var clouds: Int
+    var wind_speed: Double
+    var wind_deg: Int
+    var weather: [WeatherDetail]
     
+    enum CodingKeys: String, CodingKey {
+        case dt
+        case temp
+        case pressure
+        case humidity
+        case clouds
+        case weather
+        case feel_like
+        case dew_point
+        case wind_speed
+        case wind_deg
+    }
+    
+    init() {
+        dt = 0
+        temp = 0.0
+        feel_like = 0.0
+        pressure = 0
+        humidity = 0
+        clouds = 0
+        wind_speed = 0
+        wind_deg = 0
+        weather = []
+    }
+}
+
+extension Weather {
+    var id: UUID {
+        return UUID()
+    }
 }
