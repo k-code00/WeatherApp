4260ef2ee97189183ba0ae4d09ce53c7fa398807
kojo <kojo.abakah@techconsulting.tech>
2023-10-25 08:48:20 -0400

    updated view file

--- WeatherApp/Views/LottieView.swift
+++ WeatherApp/Views/LottieView.swift
@@ -9,29 +9,48 @@ import SwiftUI
 import Lottie
 
 struct LottieView: UIViewRepresentable {
+    // The name of the Lottie animation file (without the .json extension).
     var name: String
+    
+    // The loop mode for the Lottie animation.
     var loopMode: LottieLoopMode = .loop
     
+    // UIView to display the Lottie animation.
     func makeUIView(context: UIViewRepresentableContext<LottieView>) -> some UIView {
         let view = UIView()
         return view
     }
     
+    // UIView with the Lottie animation.
     func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<LottieView>) {
         uiView.subviews.forEach({ $0.removeFromSuperview()})
         
+        // Initialize the Lottie view.
         let animationView = LottieAnimationView()
+        
+        // Allow for AutoLayout constraints.
         animationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the Lottie view as a subview.
         uiView.addSubview(animationView)
         
+        // Set constraints to make the animation view fill its parent.
         NSLayoutConstraint.activate([
             animationView.widthAnchor.constraint(equalTo: uiView.widthAnchor),
             animationView.heightAnchor.constraint(equalTo: uiView.heightAnchor)
         ])
         
+        // Set the animation to the Lottie view.
         animationView.animation = LottieAnimation.named(name)
+        
+        // Adjust the content mode for the animation.
         animationView.contentMode = .scaleAspectFit
+        
+        // Set the loop mode for the animation.
         animationView.loopMode = loopMode
+        
+        // Start playing the animation.
         animationView.play()
     }
 }
+
