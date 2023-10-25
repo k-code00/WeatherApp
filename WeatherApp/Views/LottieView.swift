50eaa572416a5abae6500ea5cc12ec0995f6647b
kojo <kojo.abakah@techconsulting.tech>
2023-10-25 08:43:32 -0400

    added animation files

--- WeatherApp/Views/LottieView.swift
+++ WeatherApp/Views/LottieView.swift
@@ -5,4 +5,33 @@
 //  Created by Kojo on 25/10/2023.
 //
 
-import Foundation
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> some UIView {
+        let view = UIView()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<LottieView>) {
+        uiView.subviews.forEach({ $0.removeFromSuperview()})
+        
+        let animationView = LottieAnimationView()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        uiView.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: uiView.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: uiView.heightAnchor)
+        ])
+        
+        animationView.animation = LottieAnimation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+    }
+}
