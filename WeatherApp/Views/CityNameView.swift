0694e5480ca3d28e6f487ed3c2726236aa62a6b4
kojo <kojo.abakah@techconsulting.tech>
2023-10-25 09:03:16 -0400

    added view for city

---
+++ WeatherApp/Views/CityNameView.swift
@@ -0,0 +1,28 @@
+//
+//  CityNameView.swift
+//  WeatherApp
+//
+//  Created by Consultant on 25/10/2023.
+//
+
+import SwiftUI
+
+struct CityNameView: View {
+    var city: String
+    var date: String
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .center, spacing: 10) {
+                Text(city)
+                    .font(.title)
+                    .bold()
+                Text(date)
+            }.foregroundColor(.white)
+        }
+    }
+}
+
+//#Preview {
+//    CityNameView()
+//}
